4e04047ac60d23f48c1d1f344d6db007616807a6
kvnsu <k.v.n.such@gmail.com>
2020-09-18 12:45:58 -0300

    Correçoes menu home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import {Router} from "@angular/router";
 import {Session} from "../session";
 import {VideoService} from "../video.service";
 import {VideoType} from "../model/VideoType";
+import {environment} from "../../environments/environment";
 
 @Component({
   selector: 'app-home',
@@ -14,6 +15,13 @@ export class HomeComponent implements OnInit {
 
   constructor(private router:Router,public session:Session, private videoService:VideoService) { }
   videoType: VideoType[];
+
+  getImageSRC(){
+    if(environment.production){
+      return '/'+window.location.pathname.split("/")[1]+'/assets/img/folder.png';
+    }
+    return '/assets/img/folder.png';
+  }
 
   ngOnInit(): void {
     this.videoService.getListTypes().subscribe((response: JsonResponse)=>{
