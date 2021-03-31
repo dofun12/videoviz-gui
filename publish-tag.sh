e73b2e0ed250cda5981509c751f5e0834ebd3007
dofun12 <k.v.n.such@gmail.com>
2021-03-31 16:43:45 -0300

    publish tag

--- publish-tag.sh
+++ publish-tag.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-npm version patch
+npm --no-git-tag-version version release
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 git tag -l "$PACKAGE_VERSION"
 git push
-NOVO_RELEASE=$(npm version prepatch)
+NOVO_RELEASE=$(npm --no-git-tag-version version prerelease --preid snapshot)
 git add package.json
 git add package-lock.json
 git commit -m 'Novo release $NOVO_RELEASE'
