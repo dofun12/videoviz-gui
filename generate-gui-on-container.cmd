eca18e3f7041080c32bc335a9fd77be25637a224
kvnsu <k.v.n.such@gmail.com>
2022-11-01 14:27:34 -0300

    Alterando para ver se vai

--- generate-gui-on-container.cmd
+++ generate-gui-on-container.cmd
@@ -2,7 +2,7 @@ docker run ^
  --rm -it ^
   -v D:\IdeaProjects\videoviz\videoviz-gui\src:/app/src ^
   -v D:\IdeaProjects\videoviz\videoviz-gui\gui:/app/gui ^
-   node-14:latest node_modules/.bin/ng build --prod --output-path gui --base-href /gui/"
+   lemanoman/node-14:latest node_modules/.bin/ng build --prod --output-path gui --base-href /gui/"
 
 
 #".node_modules/bin/ng build --prod --output-path gui --base-href /gui/"
